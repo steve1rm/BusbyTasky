014c24775124fdd3a0be417e3851ead5db24346f
Steve <steve@kitkat@gmail.com>
2023-02-17 22:53:28 +0700

    added plugin for refresh version

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,6 +10,10 @@ pluginManagement {
     }
 }
 
+plugins {
+    id("de.fayard.refreshVersions") version "0.51.0"
+}
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
