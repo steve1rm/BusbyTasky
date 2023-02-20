0f4f4c93736369149a80d54379c2afd251f886d2
Steve <steve@kitkat@gmail.com>
2023-02-20 21:32:03 +0700

    Changes name of the Agenda to AgendaModel

--- data/src/main/java/me/androidbox/data/remote/model/AgendaModel.kt
+++ data/src/main/java/me/androidbox/data/remote/model/AgendaModel.kt
@@ -2,7 +2,7 @@ package me.androidbox.data.remote.model
 
 import me.androidbox.data.remote.model.request.EventRequestModel
 
-data class Agenda(
+data class AgendaModel(
     val listOfEvent: List<EventRequestModel>,
     val listOfTask: List<TaskModel>,
     val listOfReminder: List<ReminderModel>
