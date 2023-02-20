9b158198772c516ea1ed1cad4aa92932b1f82470
Steve <steve@kitkat@gmail.com>
2023-02-20 21:11:26 +0700

    Adds eventDao

---
+++ data/src/main/java/me/androidbox/data/local/dao/EventDao.kt
@@ -0,0 +1,21 @@
+package me.androidbox.data.local.dao
+
+import androidx.room.*
+import kotlinx.coroutines.flow.Flow
+import me.androidbox.data.local.DatabaseConstant
+import me.androidbox.data.model.EventModel
+
+@Dao
+interface EventDao {
+    @Query("SELECT * FROM ${DatabaseConstant.EVENT_TABLE}")
+    fun getEvent(): Flow<List<EventModel>>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    fun insertEvent(eventModel: EventModel)
+
+    @Update
+    fun updateEvent(eventModel: EventModel)
+
+    @Delete
+    fun deleteEvent(eventModel: EventModel)
+}
