bafdddffae7df85c7ca7d05e57752c6b922bb5ae
Steve <steve@kitkat@gmail.com>
2023-02-25 16:32:11 +0700

    Adds string reminders duration  and bell vector

---
+++ component/src/main/java/me/androidbox/component/agenda/AlarmReminder.kt
@@ -0,0 +1,19 @@
+package me.androidbox.component.agenda
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import me.androidbox.component.ui.theme.BusbyTaskyTheme
+
+@Composable
+fun AlarmReminder() {
+
+}
+
+@Composable
+@Preview(showBackground = true, name = "Alarm reminder")
+fun PreviewAlarmReminder() {
+    BusbyTaskyTheme {
+        AlarmReminder()
+    }
+}
+
