8de5660b14501d5bbf2762b9225ff97aa6965824
Steve <steve@kitkat@gmail.com>
2023-03-05 19:04:19 +0700

    Adds the @Inject constructor

--- presentation/src/main/java/me/androidbox/presentation/login/viewmodel/LoginViewModel.kt
+++ presentation/src/main/java/me/androidbox/presentation/login/viewmodel/LoginViewModel.kt
@@ -5,9 +5,10 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
 @HiltViewModel
-class LoginViewModel : ViewModel() {
+class LoginViewModel @Inject constructor() : ViewModel() {
 
     var username by mutableStateOf("")
         private set
