1a1822ac7d61ac70165d8c9380554e28aaabf8f5
Steve <steve@kitkat@gmail.com>
2023-03-07 22:27:49 +0700

    Reverted change to AuthenticationService

--- data/src/main/java/me/androidbox/data/remote/network/authentication/AuthenticationService.kt
+++ data/src/main/java/me/androidbox/data/remote/network/authentication/AuthenticationService.kt
@@ -2,23 +2,17 @@ package me.androidbox.data.remote.network.authentication
 
 import me.androidbox.data.remote.model.request.LoginRequestDto
 import me.androidbox.data.remote.model.request.RegistrationRequestDto
+import me.androidbox.data.remote.model.response.ErrorResponseDto
 import me.androidbox.data.remote.model.response.LoginDto
 import me.androidbox.data.remote.network.EndPointConstant
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
-import retrofit2.http.Query
 
 interface AuthenticationService {
     @POST(EndPointConstant.REGISTER)
     suspend fun register(
         @Body registrationRequestDto: RegistrationRequestDto)
-
-    @POST(EndPointConstant.REGISTER)
-    suspend fun register(@Query("fullName") fullName: String,
-                         @Query("email") email: String,
-                         @Query("password") password: String
-    )
 
     @POST(EndPointConstant.LOGIN)
     suspend fun login(
