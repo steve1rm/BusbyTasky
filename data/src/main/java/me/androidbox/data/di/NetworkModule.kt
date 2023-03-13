a294c9a75a37a84affcd3341cefb931655f4b34d
Steve <steve@kitkat@gmail.com>
2023-03-13 20:59:09 +0700

    Adds PreferenceRepostory to the NetworkModule and uses the SharePreferences to get the token from it

--- data/src/main/java/me/androidbox/data/di/NetworkModule.kt
+++ data/src/main/java/me/androidbox/data/di/NetworkModule.kt
@@ -1,15 +1,19 @@
 package me.androidbox.data.di
 
+import android.content.Context
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import me.androidbox.data.BuildConfig
 import me.androidbox.data.remote.interceptor.ApiKeyInterceptor
 import me.androidbox.data.remote.interceptor.TokenInterceptor
 import me.androidbox.data.remote.network.authentication.AuthenticationService
+import me.androidbox.data.remote.preference.PreferenceRepositoryImp
+import me.androidbox.domain.authentication.preference.PreferenceRepository
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
@@ -42,8 +46,14 @@ object NetworkModule {
 
     @Singleton
     @Provides
-    fun provideTokenInterceptor(): TokenInterceptor {
-        return TokenInterceptor()
+    fun providesPreferenceRepositoryImp(@ApplicationContext context: Context): PreferenceRepositoryImp {
+        return PreferenceRepositoryImp(context)
+    }
+
+    @Singleton
+    @Provides
+    fun provideTokenInterceptor(preferenceRepositoryImp: PreferenceRepositoryImp): TokenInterceptor {
+        return TokenInterceptor(preferenceRepositoryImp)
     }
 
     @Singleton
--- data/src/main/java/me/androidbox/data/remote/interceptor/TokenInterceptor.kt
+++ data/src/main/java/me/androidbox/data/remote/interceptor/TokenInterceptor.kt
@@ -1,18 +1,31 @@
 package me.androidbox.data.remote.interceptor
 
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import me.androidbox.domain.authentication.model.LoginUser
+import me.androidbox.domain.authentication.preference.PreferenceRepository
 import okhttp3.Interceptor
 import okhttp3.Response
+import kotlin.coroutines.EmptyCoroutineContext
 
-class TokenInterceptor : Interceptor {
+class TokenInterceptor(preferenceRepository: PreferenceRepository) : Interceptor {
 
     private companion object {
         const val AUTHORIZATION_HEADER = "Authorization"
+    }
+    private var loginUser: LoginUser? = null
+
+    init {
+        CoroutineScope(EmptyCoroutineContext).launch {
+            loginUser = preferenceRepository.retrieveCurrentUserOrNull()
+        }
     }
 
     override fun intercept(chain: Interceptor.Chain): Response {
         val request = chain.request()
             .newBuilder()
-            .addHeader(AUTHORIZATION_HEADER, "")
+            .addHeader(AUTHORIZATION_HEADER, loginUser?.token ?: "")
             .build()
 
         return chain.proceed(request)
