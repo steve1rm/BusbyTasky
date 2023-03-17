899881485eec59f73174f7928e24b7b822dce7e5
Steve <steve@kitkat@gmail.com>
2023-03-17 22:06:43 +0700

    Adds agenda screen

---
+++ presentation/src/main/java/me/androidbox/presentation/agenda/screen/AgendaScreen.kt
@@ -0,0 +1,19 @@
+package me.androidbox.presentation.agenda.screen
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import me.androidbox.presentation.ui.theme.BusbyTaskyTheme
+
+@Composable
+fun AgendaScreen() {
+    
+}
+
+
+@Composable
+@Preview(showBackground = true)
+fun PreviewAgendaScreen() {
+    BusbyTaskyTheme {
+        AgendaScreen()
+    }
+}
