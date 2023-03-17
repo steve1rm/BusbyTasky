415ae6b081e83b06bbcdf100430be856fd585797
Steve <steve@kitkat@gmail.com>
2023-03-17 11:42:03 +0700

    1) Adds condition on adding token to interceptor if the model is null 2) Fixes usecase error in UseCaseModule

--- data/src/main/java/me/androidbox/data/remote/interceptor/TokenInterceptor.kt
+++ data/src/main/java/me/androidbox/data/remote/interceptor/TokenInterceptor.kt
@@ -1,26 +1,22 @@
 package me.androidbox.data.remote.interceptor
 
-import me.androidbox.domain.authentication.model.AuthenticatedUser
 import me.androidbox.domain.authentication.preference.PreferenceRepository
 import okhttp3.Interceptor
 import okhttp3.Response
 
-class TokenInterceptor(preferenceRepository: PreferenceRepository) : Interceptor {
+class TokenInterceptor(private val preferenceRepository: PreferenceRepository) : Interceptor {
 
     private companion object {
         const val AUTHORIZATION_HEADER = "Authorization"
     }
-    private var loginUser: AuthenticatedUser? = null
-
-    init {
-        loginUser = preferenceRepository.retrieveCurrentUserOrNull()
-    }
 
     override fun intercept(chain: Interceptor.Chain): Response {
-        return if(!loginUser?.token.isNullOrEmpty()) {
+        val loginUser = preferenceRepository.retrieveCurrentUserOrNull()
+
+        return if(loginUser != null) {
             val request = chain.request()
                 .newBuilder()
-                .addHeader(AUTHORIZATION_HEADER, loginUser?.token ?: "")
+                .addHeader(AUTHORIZATION_HEADER, "Bearer ${loginUser.token}")
                 .build()
 
             chain.proceed(request)
