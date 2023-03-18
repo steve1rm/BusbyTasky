cbad5fbc3392da74fbafef50d2c11aa2229c48e6
Steve <steve@kitkat@gmail.com>
2023-03-18 23:24:13 +0700

    Removed unused import

--- component/src/main/java/me/androidbox/component/general/TaskButton.kt
+++ component/src/main/java/me/androidbox/component/general/TaskButton.kt
@@ -8,7 +8,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
@@ -16,8 +15,8 @@ import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import me.androidbox.component.ui.theme.BusbyTaskyTheme
-import me.androidbox.component.ui.theme.loginTextColor
 import me.androidbox.component.ui.theme.buttonColor
+import me.androidbox.component.ui.theme.loginTextColor
 
 @Composable
 fun TaskButton(
