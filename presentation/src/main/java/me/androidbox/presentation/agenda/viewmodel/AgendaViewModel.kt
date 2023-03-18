085bcf167d622c74e94de40d57f42ccf838dbebf
Steve <steve@kitkat@gmail.com>
2023-03-18 15:35:07 +0700

    Fetches the authenticated user and extracts the initials and sets the profile button with the intials

--- presentation/src/main/java/me/androidbox/presentation/agenda/viewmodel/AgendaViewModel.kt
+++ presentation/src/main/java/me/androidbox/presentation/agenda/viewmodel/AgendaViewModel.kt
@@ -1,19 +1,28 @@
 package me.androidbox.presentation.agenda.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import me.androidbox.domain.authentication.preference.PreferenceRepository
 import me.androidbox.presentation.agenda.screen.AgendaScreenEvent
 import me.androidbox.presentation.agenda.screen.AgendaScreenState
 import javax.inject.Inject
 
 @HiltViewModel
-class AgendaViewModel @Inject constructor() : ViewModel() {
+class AgendaViewModel @Inject constructor(
+    private val preferenceRepository: PreferenceRepository
+) : ViewModel() {
 
     private val _agendaScreenState = MutableStateFlow(AgendaScreenState())
     val agendaScreenState = _agendaScreenState.asStateFlow()
+
+    init {
+        getAuthenticatedUser()
+    }
 
     fun onAgendaScreenEvent(agendaScreenEvent: AgendaScreenEvent) {
         when(agendaScreenEvent) {
@@ -31,5 +40,28 @@ class AgendaViewModel @Inject constructor() : ViewModel() {
 
             }
         }
+    }
+
+    private fun getAuthenticatedUser() {
+        viewModelScope.launch {
+            preferenceRepository.retrieveCurrentUserOrNull()?.let { authenticatedUser ->
+                _agendaScreenState.update { agendaScreenState ->
+                    agendaScreenState.copy(
+                        usersInitials = authenticatedUser.fullName.extractInitials()
+                    )
+                }
+            }
+        }
+    }
+
+    private fun String.extractInitials() : String {
+        val listOfFullName = this.split(" ")
+        var initials = ""
+
+        listOfFullName.forEach { name ->
+            initials += name.first()
+        }
+
+        return initials.uppercase()
     }
 }
