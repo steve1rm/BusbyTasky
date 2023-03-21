d6cb52dd0301f5d72ca323816d4a0c61079f453c
Steve <steve@kitkat@gmail.com>
2023-03-21 13:30:50 +0700

    Updating dropdownmenu

--- component/src/main/java/me/androidbox/component/general/AgendaDropDownMenu.kt
+++ component/src/main/java/me/androidbox/component/general/AgendaDropDownMenu.kt
@@ -3,7 +3,7 @@ package me.androidbox.component.general
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
@@ -11,23 +11,26 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import me.androidbox.component.R
-import me.androidbox.component.ui.theme.*
+import me.androidbox.component.ui.theme.BusbyTaskyTheme
+import me.androidbox.component.ui.theme.divider
+import me.androidbox.component.ui.theme.dropDownMenuBackgroundColor
+import me.androidbox.component.ui.theme.dropDownMenuColor
 
 @Composable
 fun AgendaDropDownMenu(
     modifier: Modifier = Modifier,
-    isExpanded: MutableState<Boolean> = mutableStateOf(false),
+    isExpanded: Boolean,
+    onCloseDropdown: () -> Unit,
     onOpenClicked: () -> Unit,
     onEditClicked: () -> Unit,
     onDeleteClicked: () -> Unit
 ) {
 
     DropdownMenu(
-        modifier = modifier
-            .background(color = MaterialTheme.colorScheme.dropDownMenuBackgroundColor),
-        expanded = isExpanded.value,
+        modifier = modifier,
+        expanded = isExpanded,
         onDismissRequest = {
-            isExpanded.value = false
+            onCloseDropdown()
         }
     ) {
         DropdownMenuItem(
@@ -84,12 +87,10 @@ fun AgendaDropDownMenu(
 fun PreviewAgendaDropDownMenu() {
     BusbyTaskyTheme {
 
-        val isExpanded = remember {
-            mutableStateOf(true)
-        }
-
         AgendaDropDownMenu(
-            isExpanded = isExpanded,
+            modifier = Modifier.background(color = MaterialTheme.colorScheme.dropDownMenuBackgroundColor),
+            isExpanded = true,
+            onCloseDropdown = { },
             onOpenClicked  = { },
             onEditClicked = { },
             onDeleteClicked = { }
