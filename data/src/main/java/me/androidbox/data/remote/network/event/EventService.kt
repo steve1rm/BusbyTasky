53bfadcf6feddf1c9e7ba7061df3156ecf456071
Steve <steve@kitkat@gmail.com>
2023-03-23 14:48:01 +0700

    Adds the multipart parameters to the event service

--- data/src/main/java/me/androidbox/data/remote/network/event/EventService.kt
+++ data/src/main/java/me/androidbox/data/remote/network/event/EventService.kt
@@ -4,19 +4,15 @@ import me.androidbox.data.remote.model.request.EventRequestDto
 import me.androidbox.data.remote.model.request.EventUpdateRequestDto
 import me.androidbox.data.remote.model.response.EventDto
 import me.androidbox.data.remote.network.EndPointConstant
-import retrofit2.http.Body
-import retrofit2.http.DELETE
-import retrofit2.http.GET
-import retrofit2.http.Multipart
-import retrofit2.http.POST
-import retrofit2.http.PUT
-import retrofit2.http.Query
+import okhttp3.MultipartBody
+import retrofit2.http.*
 
 interface EventService {
     @Multipart
     @POST(EndPointConstant.EVENT)
     suspend fun createEvent(
-        @Body eventRequestDto: EventRequestDto
+        @Part photoFile: MultipartBody.Part,
+        @Part eventRequestDto: EventRequestDto
     ): EventDto
 
     @GET(EndPointConstant.EVENT)
@@ -32,6 +28,7 @@ interface EventService {
     @Multipart
     @PUT(EndPointConstant.EVENT)
     suspend fun updateEvent(
-        @Body eventUpdateRequestDto: EventUpdateRequestDto
+        @Part photoFile: MultipartBody.Part,
+        @Part eventUpdateRequestDto: EventUpdateRequestDto
     ): EventDto
 }
