3789946a057e8fa28305bfd402900e906f094b8e
Steve <steve@kitkat@gmail.com>
2023-03-25 00:02:59 +0700

    Removed code from HomeActivity

--- presentation/src/main/java/me/androidbox/presentation/HomeActivity.kt
+++ presentation/src/main/java/me/androidbox/presentation/HomeActivity.kt
@@ -50,10 +50,6 @@ class HomeActivity : ComponentActivity() {
                 }
             }
 
-            val eventState by derivedStateOf {
-                val downloadEvent = downloadInfo?.outputData?.getString("")?.toUri()
-            }
-
             /** TODO Just testing using work manager to send a event request */
             LaunchedEffect(key1 = true, block = {
                 workManager.beginUniqueWork("upload_event", ExistingWorkPolicy.KEEP, uploadEventWorkerRequest).enqueue()
