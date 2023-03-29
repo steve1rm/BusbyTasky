44d241bcc38cfa1a4e225c91e34e028abdb34a02
Steve <steve@kitkat@gmail.com>
2023-03-29 15:36:13 +0700

    Adds the state and event for saving the content of the title and description of the agenda item. Uses  to copy the state

--- presentation/src/main/java/me/androidbox/presentation/event/screen/EventScreenEvent.kt
+++ presentation/src/main/java/me/androidbox/presentation/event/screen/EventScreenEvent.kt
@@ -6,4 +6,6 @@ import me.androidbox.component.agenda.VisitorType
 sealed interface EventScreenEvent {
     data class OnPhotoUriAdded(val photoUri: Uri): EventScreenEvent
     data class OnSelectedVisitorType(val visitorType: VisitorType): EventScreenEvent
+
+    data class OnSaveEditOrDescriptionContent(val content: String): EventScreenEvent
 }
