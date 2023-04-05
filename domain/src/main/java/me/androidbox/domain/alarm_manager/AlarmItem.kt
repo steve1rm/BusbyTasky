d5e963ec413eeab138b9b9bd5e319aef4ec2a2ca
Steve <steve@kitkat@gmail.com>
2023-04-05 16:15:13 +0700

    Renamed to remindAtInMilliSecs = this.remindAt * 1000

--- domain/src/main/java/me/androidbox/domain/alarm_manager/AlarmItem.kt
+++ domain/src/main/java/me/androidbox/domain/alarm_manager/AlarmItem.kt
@@ -6,7 +6,7 @@ data class AlarmItem(
     val agendaId: String,
     val title: String,
     val description: String,
-    val remindAt: Long,
+    val remindAtInMilliSecs: Long,
     val agendaType: AgendaType
 )
 
@@ -15,7 +15,7 @@ fun Event.toAlarmItem(agendaType: AgendaType): AlarmItem {
         agendaId = this.id,
         title = this.title,
         description = this.description,
-        remindAt = this.remindAt * 1000,
+        remindAtInMilliSecs = this.remindAt * 1000,
         agendaType = agendaType
     )
 }
