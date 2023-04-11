e2e590737daa661f5c03a3980ec54d791436b7f5
Steve <steve@kitkat@gmail.com>
2023-04-11 14:26:33 +0700

    Updated to return null if the attendee doesn't exist

--- domain/src/main/java/me/androidbox/domain/event/usecase/VerifyVisitorEmailUseCase.kt
+++ domain/src/main/java/me/androidbox/domain/event/usecase/VerifyVisitorEmailUseCase.kt
@@ -1,7 +1,8 @@
 package me.androidbox.domain.event.usecase
 
 import me.androidbox.domain.authentication.ResponseState
+import me.androidbox.domain.authentication.model.Attendee
 
 interface VerifyVisitorEmailUseCase {
-    suspend fun execute(email: String): ResponseState<Boolean>
+    suspend fun execute(email: String): ResponseState<Attendee?>
 }
