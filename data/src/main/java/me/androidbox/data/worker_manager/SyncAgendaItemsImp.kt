afa4811e7385a94e38a91b1f5f347823f51bfdfe
Steve <steve@kitkat@gmail.com>
2023-04-17 14:24:00 +0700

    Updates the work manager so that it uses the dao directly to get the deleted items and then deleting them after finishing, rather than passing them as parameters

--- data/src/main/java/me/androidbox/data/worker_manager/SyncAgendaItemsImp.kt
+++ data/src/main/java/me/androidbox/data/worker_manager/SyncAgendaItemsImp.kt
@@ -12,31 +12,14 @@ import javax.inject.Inject
 
 class SyncAgendaItemsImp @Inject constructor(
     private val workManager: WorkManager,
-    private val eventDao: EventDao,
-    private val taskDao: TaskDao,
-    private val reminderDao: ReminderDao
 ) : SyncAgendaItems {
 
     companion object {
-        const val DELETED_EVENT_IDS = "deletedEventIds"
-        const val DELETED_TASK_IDS = "deletedTaskIds"
-        const val DELETED_REMINDER_IDS = "deletedReminderIds"
         const val THIRTY_MINUTES = 30L
         const val SYNC_DELETED_AGENDA_IDS = "syncDeletedAgendaIds"
     }
 
     override suspend fun sync(): UUID {
-
-        /* Use eventDao to get the deleted items */
-        val deletedEventIds = eventDao.getAllEventsBySyncType(SyncAgendaType.DELETE)
-        val deletedTaskIds = taskDao.getAllTasksBySyncType(SyncAgendaType.DELETE)
-        val deletedReminderIds = reminderDao.getAllRemindersBySyncType(SyncAgendaType.DELETE)
-
-        val deletedAgendaItemsIds = workDataOf(
-            DELETED_EVENT_IDS to deletedEventIds.toTypedArray(),
-            DELETED_TASK_IDS to deletedTaskIds.toTypedArray(),
-            DELETED_REMINDER_IDS to deletedReminderIds.toTypedArray()
-        )
 
         val syncWorkerRequest = PeriodicWorkRequestBuilder<SyncAgendaItemsWorker>(
             repeatInterval = THIRTY_MINUTES,
@@ -46,7 +29,6 @@ class SyncAgendaItemsImp @Inject constructor(
                 BackoffPolicy.EXPONENTIAL,
                 WorkRequest.MIN_BACKOFF_MILLIS,
                 TimeUnit.MILLISECONDS)
-            .setInputData(deletedAgendaItemsIds)
             .build()
 
         workManager.enqueueUniquePeriodicWork(
--- data/src/main/java/me/androidbox/data/worker_manager/SyncAgendaItemsWorker.kt
+++ data/src/main/java/me/androidbox/data/worker_manager/SyncAgendaItemsWorker.kt
@@ -6,33 +6,54 @@ import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedInject
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.supervisorScope
+import kotlinx.coroutines.withContext
+import me.androidbox.data.local.dao.EventDao
+import me.androidbox.data.local.dao.ReminderDao
+import me.androidbox.data.local.dao.TaskDao
 import me.androidbox.data.remote.model.request.SyncAgendaDto
 import me.androidbox.data.remote.network.agenda.AgendaService
 import me.androidbox.data.remote.util.CheckResult.checkResult
-import me.androidbox.data.worker_manager.SyncAgendaItemsImp.Companion.DELETED_EVENT_IDS
-import me.androidbox.data.worker_manager.SyncAgendaItemsImp.Companion.DELETED_REMINDER_IDS
-import me.androidbox.data.worker_manager.SyncAgendaItemsImp.Companion.DELETED_TASK_IDS
 import me.androidbox.data.worker_manager.constant.Constant.RETRY_COUNT
+import me.androidbox.domain.constant.SyncAgendaType
 
 @HiltWorker
 class SyncAgendaItemsWorker @AssistedInject constructor(
     @Assisted private val context: Context,
     @Assisted private val workerParameters: WorkerParameters,
+    private val eventDao: EventDao,
+    private val taskDao: TaskDao,
+    private val reminderDao: ReminderDao,
     private val agendaService: AgendaService
 ) : CoroutineWorker(context, workerParameters) {
 
     override suspend fun doWork(): Result {
-        val deletedEventIds = inputData.getStringArray(DELETED_EVENT_IDS) ?: emptyArray()
-        val deletedTaskIds = inputData.getStringArray(DELETED_TASK_IDS) ?: emptyArray()
-        val deletedReminderIds = inputData.getStringArray(DELETED_REMINDER_IDS) ?: emptyArray()
+        val deletedEventIds: Deferred<List<String>>
+        val deletedTaskIds: Deferred<List<String>>
+        val deletedReminderIds: Deferred<List<String>>
 
+        supervisorScope {
+            deletedEventIds = async {
+                eventDao.getAllEventsBySyncType(SyncAgendaType.DELETE)
+            }
+
+            deletedTaskIds = async {
+                taskDao.getAllTasksBySyncType(SyncAgendaType.DELETE)
+            }
+
+            deletedReminderIds = async {
+                reminderDao.getAllRemindersBySyncType(SyncAgendaType.DELETE)
+            }
+        }
 
         val responseResult = checkResult {
             val syncAgendaDto = SyncAgendaDto(
-                deletedEventIds = deletedEventIds.toList(),
-                deletedTaskIds = deletedTaskIds.toList(),
-                deletedReminderIds = deletedReminderIds.toList())
-
+                deletedEventIds = deletedEventIds.await().toList(),
+                deletedTaskIds = deletedTaskIds.await().toList(),
+                deletedReminderIds = deletedReminderIds.await().toList())
             agendaService.syncAgenda(syncAgendaDto)
         }
 
@@ -42,6 +63,7 @@ class SyncAgendaItemsWorker @AssistedInject constructor(
 
         val result = responseResult.fold(
             onSuccess = {
+                deleteAllDeletedSyncAgendaItems()
                 Result.success()
             },
             onFailure = {
@@ -50,5 +72,13 @@ class SyncAgendaItemsWorker @AssistedInject constructor(
         )
 
         return result
+    }
+
+    private suspend fun deleteAllDeletedSyncAgendaItems() {
+        withContext(Dispatchers.IO) {
+            eventDao.deleteSyncEventsBySyncType(SyncAgendaType.DELETE)
+            taskDao.deleteSyncTasksBySyncType(SyncAgendaType.DELETE)
+            reminderDao.deleteSyncRemindersBySyncType(SyncAgendaType.DELETE)
+        }
     }
 }
