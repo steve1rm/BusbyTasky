733345a3a18c6eafdce00da933fa74e1dd55446c
Steve <steve@kitkat@gmail.com>
2023-04-30 18:06:36 +0700

    Adds headlineMedium to the register screen

--- presentation/src/main/java/me/androidbox/presentation/login/screen/RegisterScreen.kt
+++ presentation/src/main/java/me/androidbox/presentation/login/screen/RegisterScreen.kt
@@ -16,6 +16,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -66,11 +67,9 @@ fun RegisterScreen(
 
         Text(
             modifier = Modifier.fillMaxWidth(),
-            text = "Create your account",
-            color = Color.White,
-            fontSize = 28.sp,
-            fontWeight = FontWeight.Bold,
-            textAlign = TextAlign.Center
+            text = stringResource(me.androidbox.presentation.R.string.create_your_account),
+            textAlign = TextAlign.Center,
+            style = MaterialTheme.typography.headlineMedium
         )
 
         Spacer(modifier = Modifier.height(42.dp))
