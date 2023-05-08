62f5cdd07b51066059df0fd69a25a69d44ee32cb
Steve <steve@kitkat@gmail.com>
2023-05-08 15:44:40 +0700

    Adds the supervisorscope when fetching the tasks and reminders

--- data/src/main/java/me/androidbox/data/worker_manager/TaskCreateSynchronizerWorker.kt
+++ data/src/main/java/me/androidbox/data/worker_manager/TaskCreateSynchronizerWorker.kt
@@ -6,14 +6,20 @@ import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedInject
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.async
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.supervisorScope
 import me.androidbox.data.local.dao.ReminderDao
 import me.androidbox.data.local.dao.TaskDao
+import me.androidbox.data.local.entity.ReminderEntity
+import me.androidbox.data.local.entity.TaskEntity
 import me.androidbox.data.mapper.toTaskDto
+import me.androidbox.data.remote.network.reminder.ReminderService
 import me.androidbox.data.remote.network.task.TaskService
 import me.androidbox.data.remote.util.CheckResult.checkResult
 import me.androidbox.data.worker_manager.constant.Constant.RETRY_COUNT
+import me.androidbox.domain.agenda.model.Task
 import me.androidbox.domain.constant.SyncAgendaType
 
 @HiltWorker
@@ -22,7 +28,7 @@ class TaskCreateSynchronizerWorker @AssistedInject constructor(
     @Assisted private val workerParameters: WorkerParameters,
     private val taskService: TaskService,
     private val taskDao: TaskDao,
-    private val reminderService: reminderService,
+    private val reminderService: ReminderService,
     private val reminderDao: ReminderDao,
 ) : CoroutineWorker(context, workerParameters) {
 
@@ -35,8 +41,82 @@ class TaskCreateSynchronizerWorker @AssistedInject constructor(
             Result.retry()
         }
 
-        /** Get all the created tasks */
-        val createdTasks = taskDao.getAllTasksBySyncType(SyncAgendaType.CREATE)
+        val createdTasks: Deferred<List<TaskEntity>>
+        val updatedTasks: Deferred<List<TaskEntity>>
+        val createdReminders: Deferred<List<ReminderEntity>>
+        val updatedReminders: Deferred<List<ReminderEntity>>
+
+        /** Get all the created, updated tasks and reminders
+         *  TODO As these will run sequentially - is it better to use supervisor scope and run them in parallel? */
+        supervisorScope {
+            createdTasks = async {
+                val taskIds = taskDao.getAllTasksBySyncType(SyncAgendaType.CREATE)
+
+                taskIds.map { id ->
+                    taskDao.getTaskById(id)
+                }
+            }
+
+            updatedTasks = async {
+                val taskIds = taskDao.getAllTasksBySyncType(SyncAgendaType.UPDATE)
+
+                taskIds.map { id ->
+                    taskDao.getTaskById(id)
+                }
+            }
+
+            createdReminders = async {
+                val reminderIds = reminderDao.getAllRemindersBySyncType(SyncAgendaType.CREATE)
+
+                reminderIds.map { id ->
+                    reminderDao.getReminderById(id)
+                }
+            }
+
+            updatedReminders = async {
+                val reminderIds = reminderDao.getAllRemindersBySyncType(SyncAgendaType.UPDATE)
+
+                reminderIds.map { id ->
+                    reminderDao.getReminderById(id)
+                }
+            }
+        }
+
+        createdTasks.await()
+        updatedTasks.await()
+        createdReminders.await()
+        updatedReminders.await()
+
+
+        val responseState = checkResult{
+            supervisorScope {
+                val createTaskJob = launch {
+                    createdTasks.map { taskEntity ->
+                        /* Create the task on remote */
+                        taskService.createTask(taskEntity.toTaskDto())
+                        /* If success will run this delete - else if loop next next one */
+                        taskDao.deleteSyncTaskById(taskEntity.id)
+                    }
+                }
+
+                val updateTaskJob = launch {
+
+                }
+
+                val createReminderJob = launch {
+
+                }
+
+                val updateReminderJob = launch {
+
+                }
+
+                createTaskJob.join()
+                updateTaskJob.join()
+                createReminderJob.join()
+                updateReminderJob.join()
+            }
+        }
 
         if (createdTasks.isNotEmpty()) {
             val responseState = checkResult{
@@ -56,6 +136,8 @@ class TaskCreateSynchronizerWorker @AssistedInject constructor(
             responseState.fold(
                 onSuccess = {
                     taskDao.deleteSyncTasksBySyncType(SyncAgendaType.CREATE)
+                    taskDao.deleteSyncTasksBySyncType(SyncAgendaType.UPDATE)
+
                     Result.success()
                 },
                 onFailure = {
