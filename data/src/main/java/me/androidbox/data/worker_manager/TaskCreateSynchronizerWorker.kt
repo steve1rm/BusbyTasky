748b9d9ca0cec77c03dea95bc090f5b76476bcb6
Steve <steve@kitkat@gmail.com>
2023-05-08 16:20:22 +0700

    Updated with todo comments

--- data/src/main/java/me/androidbox/data/worker_manager/TaskCreateSynchronizerWorker.kt
+++ data/src/main/java/me/androidbox/data/worker_manager/TaskCreateSynchronizerWorker.kt
@@ -14,15 +14,16 @@ import me.androidbox.data.local.dao.ReminderDao
 import me.androidbox.data.local.dao.TaskDao
 import me.androidbox.data.local.entity.ReminderEntity
 import me.androidbox.data.local.entity.TaskEntity
+import me.androidbox.data.mapper.toReminderDto
 import me.androidbox.data.mapper.toTaskDto
 import me.androidbox.data.remote.network.reminder.ReminderService
 import me.androidbox.data.remote.network.task.TaskService
 import me.androidbox.data.remote.util.CheckResult.checkResult
 import me.androidbox.data.worker_manager.constant.Constant.RETRY_COUNT
-import me.androidbox.domain.agenda.model.Task
 import me.androidbox.domain.constant.SyncAgendaType
 
 @HiltWorker
+/** TODO Rename to something more readable as this is not just related to tasks */
 class TaskCreateSynchronizerWorker @AssistedInject constructor(
     @Assisted private val context: Context,
     @Assisted private val workerParameters: WorkerParameters,
@@ -34,6 +35,7 @@ class TaskCreateSynchronizerWorker @AssistedInject constructor(
 
     override suspend fun doWork(): Result {
 
+        /** TODO is this the correct place to have this check and to retry or fail */
         if(runAttemptCount > RETRY_COUNT) {
             Result.failure()
         }
@@ -82,6 +84,9 @@ class TaskCreateSynchronizerWorker @AssistedInject constructor(
             }
         }
 
+        /** TODO I am using supervisor scope to launch so if one fails the other will still run
+         *  However, will the worker return failure if a single one fails even though some failed?
+         *  My checkResult will run everything in this block */
         val responseState = checkResult {
             supervisorScope {
                 val createTaskJob = launch {
@@ -102,18 +107,19 @@ class TaskCreateSynchronizerWorker @AssistedInject constructor(
 
                 val createReminderJob = launch {
                     createdReminders.await().forEach { reminderEntity ->
-                        reminderService.createReminder(reminderEntity)
+                        reminderService.createReminder(reminderEntity.toReminderDto())
                         reminderDao.deleteSyncReminderById(reminderEntity.id)
                     }
                 }
 
                 val updateReminderJob = launch {
                     updatedReminders.await().forEach { reminderEntity ->
-                        reminderService.updateReminder(reminderEntity)
+                        reminderService.updateReminder(reminderEntity.toReminderDto())
                         reminderDao.deleteReminderById(reminderEntity.id)
                     }
                 }
 
+                /** TODO Waiting for all jobs to complete */
                 createTaskJob.join()
                 updateTaskJob.join()
                 createReminderJob.join()
@@ -121,12 +127,19 @@ class TaskCreateSynchronizerWorker @AssistedInject constructor(
             }
         }
 
-        return  responseState.fold(
+        return responseState.fold(
             onSuccess = {
+                /** TODO Success we can delete all these from the sync table
+                 *  However, I don't think I need to do this now if
+                 *  1) I am already deleting the items individually when create/updated
+                 *  2) If the worker is success, but i.e. some reminders failed to create it will empty them
+                 *     and when the worker runs again, there will be nothing to fetch.
+                 *  */
                 taskDao.deleteSyncTasksBySyncType(SyncAgendaType.CREATE)
                 taskDao.deleteSyncTasksBySyncType(SyncAgendaType.UPDATE)
                 reminderDao.deleteSyncRemindersBySyncType(SyncAgendaType.CREATE)
                 reminderDao.deleteSyncRemindersBySyncType(SyncAgendaType.UPDATE)
+
                 Result.success()
             },
             onFailure = {
