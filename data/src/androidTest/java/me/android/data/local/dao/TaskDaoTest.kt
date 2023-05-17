e439f2987d38032d0e9ccb9c857ac6b1e17216a7
Steve <steve@kitkat@gmail.com>
2023-05-17 21:32:12 +0700

    Adds TaskDaoTest

---
+++ data/src/androidTest/java/me/android/data/local/dao/TaskDaoTest.kt
@@ -0,0 +1,10 @@
+package me.android.data.local.dao
+
+import me.androidbox.data.local.database.BusbyTaskyDatabase
+
+class TaskDaoTest {
+    private lateinit var database: BusbyTaskyDatabase
+
+    @Before
+
+}
